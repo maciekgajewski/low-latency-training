5ad8824882ff708e3f67dcbf5b7d32d249d5296e
Maciej Gajewski <maciej.gajewski0@gmail.com>
2019-11-07 12:02:40 +0100

    sample becnhmark

--- maciek/benchmarks1/hello_benchmark.cc
+++ maciek/benchmarks1/hello_benchmark.cc
@@ -1,13 +1,40 @@
 #include <benchmark/benchmark.h>
 
+#include <deque>
+#include <list>
 #include <numeric>
+#include <vector>
 
-static void hello(benchmark::State& state) {
-  for(auto _ : state) {
-    // hello :)
+static void push_back_reserve(benchmark::State &state) {
+
+  int size = state.range(0);
+
+  for (auto _ : state) {
+    std::vector<int> v;
+    v.reserve(size);
+    for (int i = 0; i < size; ++i) {
+      v.push_back(i);
+      benchmark::DoNotOptimize(v.data());
+    }
   }
 }
 
-BENCHMARK(hello);
+template <typename Container> static void push_back(benchmark::State &state) {
+  int size = state.range(0);
+  for (auto _ : state) {
+    Container v;
+    for (int i = 0; i < size; ++i) {
+      v.push_back(i);
+      // benchmark::DoNotOptimize(v.front());
+    }
+  }
+}
+
+BENCHMARK_TEMPLATE(push_back, std::vector<int>)
+    ->RangeMultiplier(2)
+    ->Range(1024, 128 * 1024);
+BENCHMARK_TEMPLATE(push_back, std::list<int>)->Range(1024, 128 * 1024);
+BENCHMARK_TEMPLATE(push_back, std::deque<int>)->Range(1024, 128 * 1024);
+BENCHMARK(push_back_reserve)->Range(1024, 128 * 1024);
 
 BENCHMARK_MAIN();
