5eaced6054af46bd0df49750884fecaa9591cf6b
Maciej Gajewski <maciej.gajewski0@gmail.com>
2019-11-07 14:17:03 +0100

    arena allocator used

--- maciek/benchmarks1/hello_benchmark.cc
+++ maciek/benchmarks1/hello_benchmark.cc
@@ -1,4 +1,5 @@
 #include "mtrace.h"
+#include "short_alloc.h"
 
 #include <benchmark/benchmark.h>
 
@@ -7,6 +8,9 @@
 #include <list>
 #include <numeric>
 #include <vector>
+
+template <class T, std::size_t BufSize = 1024 * 1024>
+using SmallVector = std::vector<T, short_alloc<T, BufSize, alignof(T)>>;
 
 static void push_back_reserve(benchmark::State &state) {
 
@@ -38,9 +42,21 @@ template <typename Container> static void push_back(benchmark::State &state) {
   state.counters["allocations"] = m.counters().malloc_calls();
 }
 
+static void push_back_small_vector(benchmark::State &state) {
+  int size = state.range(0);
+  mtrace m;
+  for (auto _ : state) {
+    SmallVector<int>::allocator_type::arena_type arena;
+    SmallVector<int> v(arena);
+    for (int i = 0; i < size; ++i) {
+      v.push_back(i);
+    }
+  }
+  state.counters["allocations"] = m.counters().malloc_calls();
+}
+
 BENCHMARK_TEMPLATE(push_back, std::vector<int>)->Range(1024, 128 * 1024);
-// BENCHMARK_TEMPLATE(push_back, std::list<int>)->Range(1024, 128 * 1024);
-// BENCHMARK_TEMPLATE(push_back, std::deque<int>)->Range(1024, 128 * 1024);
+BENCHMARK(push_back_small_vector)->Range(1024, 128 * 1024);
 BENCHMARK(push_back_reserve)->Range(1024, 128 * 1024);
 
 BENCHMARK_MAIN();
