70f7a403a1d6539efbe66e0dc706e61997270853
Maciej Gajewski <maciej.gajewski0@gmail.com>
2019-11-07 16:54:01 +0100

    accumualte becnhmark

---
+++ maciek/benchmarks1/accumulate.cc
@@ -0,0 +1,39 @@
+#include <benchmark/benchmark.h>
+
+#include <cstdlib>
+#include <list>
+#include <numeric>
+#include <vector>
+
+void fill(std::list<int> &container, std::size_t elements) {
+  for (int i = 0; i < elements; ++i)
+    container.push_back(::rand());
+}
+
+void fill(std::vector<int> &container, std::size_t elements) {
+  container.reserve(elements);
+  for (int i = 0; i < elements; ++i)
+    container.push_back(::rand());
+}
+
+template <typename Container> static void accumulate(benchmark::State &state) {
+  Container container;
+  auto elements = state.range(0);
+
+  fill(container, elements);
+  state.SetBytesProcessed(elements * sizeof(int));
+
+  for (auto _ : state) {
+    int sum = std::accumulate(container.begin(), container.end(), 0);
+    benchmark::DoNotOptimize(&sum);
+  }
+}
+
+BENCHMARK_TEMPLATE(accumulate, std::vector<int>)
+    ->RangeMultiplier(1024)
+    ->Range(1ull << 10, 2ull << 10 << 10);
+BENCHMARK_TEMPLATE(accumulate, std::list<int>)
+    ->RangeMultiplier(1024)
+    ->Range(1ull << 10, 2ull << 10 << 10);
+
+BENCHMARK_MAIN();
