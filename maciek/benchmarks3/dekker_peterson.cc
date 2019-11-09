e958f482fc33ed0803cb78887dc601a1b8f1944e
Maciej Gajewski <maciej.gajewski0@gmail.com>
2019-11-09 15:04:28 +0100

    dekker implemented

--- maciek/benchmarks3/dekker_peterson.cc
+++ maciek/benchmarks3/dekker_peterson.cc
@@ -2,4 +2,49 @@
 #include <iostream>
 #include <thread>
 
-int main() { std::cout << "Hello" << std::endl; }
+#include <x86intrin.h>
+
+volatile int X = 0;
+volatile int Y = 0;
+volatile int r1 = 0;
+volatile int r2 = 0;
+//         std::atomic_signal_fence(std::memory_order_relaxed);
+
+std::atomic<int> threadsReady;
+
+int main() {
+  int errors = 0;
+  int iterations = 0;
+  while (true) {
+    X = 0;
+    Y = 0;
+    r1 = 0;
+    r2 = 0;
+
+    threadsReady = 0;
+
+    std::thread t1([] {
+      threadsReady++;
+      while (threadsReady < 2)
+        ;
+      X = 1;
+      r1 = Y;
+    });
+    std::thread t2([] {
+      threadsReady++;
+      while (threadsReady < 2)
+        ;
+      Y = 1;
+      r2 = X;
+    });
+
+    t1.join();
+    t2.join();
+
+    if (r1 == 0 && r2 == 0)
+      errors++;
+    iterations++;
+    if (iterations % 1000 == 0)
+      std::cout << errors << "/" << iterations << std::endl;
+  }
+}
