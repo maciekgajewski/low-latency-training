5168c118200abd27ad7e6133ee01c7b6858bb51d
Maciej Gajewski <maciej.gajewski0@gmail.com>
2019-11-09 11:37:42 +0100

    expanded sharing benchmark

--- maciek/benchmarks3/false_sharing.cc
+++ maciek/benchmarks3/false_sharing.cc
@@ -1,3 +1,5 @@
+#include "util.hh"
+
 #include <benchmark/benchmark.h>
 
 #include <algorithm>
@@ -23,30 +25,60 @@ static_assert(sizeof(SharedDataWithGap) > 64);
 static_assert(offsetof(SharedDataWithGap, b) == 64);
 
 template <typename Shared> static void sharing(benchmark::State &state) {
-
   Shared shared;
-  std::atomic<bool> go = false;
+  std::atomic<bool> go = true;
   std::generate(shared.a.begin(), shared.a.end(), std::rand);
 
   std::thread background([&] {
     while (go)
-      for (int &v : shared.b)
+      for (int &v : shared.b) {
         v = std::rand();
+        benchmark::DoNotOptimize(shared.b.data());
+      }
   });
-
   for (auto _ : state) {
-    int sum = 0;
-    for (int i = 0; i < 1000; ++i) {
-      sum += std::accumulate(shared.a.begin(), shared.a.end(), 0);
-      benchmark::DoNotOptimize(sum);
-    }
+    int sum = std::accumulate(shared.a.begin(), shared.a.end(), 0);
+    benchmark::DoNotOptimize(sum);
   }
-
   go = false;
   background.join();
 }
 
+static void justSum(benchmark::State &state) {
+
+  SharedData shared;
+  std::generate(shared.a.begin(), shared.a.end(), std::rand);
+
+  for (auto _ : state) {
+    int sum = std::accumulate(shared.a.begin(), shared.a.end(), 0);
+    benchmark::DoNotOptimize(sum);
+  }
+}
+
+template <typename Shared> static void sharedRead(benchmark::State &state) {
+
+  Shared shared;
+  std::atomic<bool> go = true;
+  std::generate(shared.a.begin(), shared.a.end(), std::rand);
+
+  std::thread background([&] {
+    while (go) {
+      int sum = std::accumulate(shared.b.begin(), shared.b.end(), 0);
+      benchmark::DoNotOptimize(&sum);
+    }
+  });
+  for (auto _ : state) {
+    int sum = std::accumulate(shared.a.begin(), shared.a.end(), 0);
+    benchmark::DoNotOptimize(sum);
+  }
+  go = false;
+  background.join();
+}
+
+BENCHMARK(justSum);
 BENCHMARK_TEMPLATE(sharing, SharedData);
 BENCHMARK_TEMPLATE(sharing, SharedDataWithGap);
+BENCHMARK_TEMPLATE(sharedRead, SharedData);
+BENCHMARK_TEMPLATE(sharedRead, SharedDataWithGap);
 
 BENCHMARK_MAIN();
