86daada937475b82d7113d7266bbd9259f5fea14
Maciej Gajewski <maciej.gajewski0@gmail.com>
2019-11-09 11:09:59 +0100

    falshe sharing, revese results

--- maciek/benchmarks3/false_sharing.cc
+++ maciek/benchmarks3/false_sharing.cc
@@ -14,17 +14,39 @@ struct alignas(64) SharedData {
 static_assert(sizeof(SharedData) == 64);
 static_assert(offsetof(SharedData, b) == 32);
 
-static void singleThread(benchmark::State &state) {
+struct alignas(64) SharedDataWithGap {
+  std::array<int, 8> a;
+  std::array<int, 8> gap;
+  std::array<int, 8> b;
+};
+static_assert(sizeof(SharedDataWithGap) > 64);
+static_assert(offsetof(SharedDataWithGap, b) == 64);
 
-  SharedData shared;
+template <typename Shared> static void sharing(benchmark::State &state) {
+
+  Shared shared;
+  std::atomic<bool> go = false;
   std::generate(shared.a.begin(), shared.a.end(), std::rand);
 
+  std::thread background([&] {
+    while (go)
+      for (int &v : shared.b)
+        v = std::rand();
+  });
+
   for (auto _ : state) {
-    int sum = std::accumulate(shared.a.begin(), shared.a.end(), 0);
-    benchmark::DoNotOptimize(sum);
+    int sum = 0;
+    for (int i = 0; i < 1000; ++i) {
+      sum += std::accumulate(shared.a.begin(), shared.a.end(), 0);
+      benchmark::DoNotOptimize(sum);
+    }
   }
+
+  go = false;
+  background.join();
 }
 
-BENCHMARK(singleThread);
+BENCHMARK_TEMPLATE(sharing, SharedData);
+BENCHMARK_TEMPLATE(sharing, SharedDataWithGap);
 
 BENCHMARK_MAIN();
